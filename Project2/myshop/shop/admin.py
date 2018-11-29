dbbe0f14f08a70f2e055fb9f600f362fe476a92f
gogabego <45129184+gogabego@users.noreply.github.com>
2018-11-29 13:23:54 -0600

    Update admin.py

--- Project2/myshop/shop/admin.py
+++ Project2/myshop/shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Category, Product
 # Register your models here.
+#hey, this is new!
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
